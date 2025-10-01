c6e4552ec43d27fecf46e0b6a204f062dfab1ede
shinsak12-sketch <shinsak12@gmail.com>
2025-10-01 18:41:30 +0900

    Update index.js

--- pages/api/admin/notices/index.js
+++ pages/api/admin/notices/index.js
@@ -1,4 +1,4 @@
-import prisma from '../../../../../lib/prisma';
+import prisma from '../../../../lib/prisma';
 const ADMIN_PASS = process.env.ADMIN_PASS || 'dbsonsa';
 
 export default async function handler(req, res) {
